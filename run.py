f16409f99a17c1abe133870d5ddc9d09dc68f809
Dimitar Milev <rokambol_88@abv.bg>
2019-04-02 11:36:10 +0000

    initial herocu commit

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 import pymysql
-from flask import Flask,  redirect, render_template, request, url_for, session, escape, flash
+from flask import Flask, json, redirect, render_template, request, url_for, session, escape, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-
+from html.parser import HTMLParser
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='THISISendLess986'
@@ -17,39 +17,31 @@ connection = pymysql.connect(host='localhost',
 							 user=username,
 							 password='',
 							 db='galaxy')
-
-#try:
-    #Run a query
-    #with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-    	#sql = "SELECT * FROM planets"
-    	#cursor.execute(sql)
-    	#result =cursor.fetchall()
-    	#print(result)
-#finally:
-	#close connectionSELECT * FROM planets JOIN images JOIN citizensFULL JOIN citizens ON name = picture_name LIMIT 9
-#	connection.close()AND 
-#
+ 
+#variables for query extract data from SQL tables
 union_table = """ SELECT * FROM planets JOIN images ON name = picture_name LIMIT 9"""
-count1 =""" SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 1
+
+#query show data from citizens table as population row
+count1 =""" SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 1
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 2
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 2
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 3
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 3
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 4
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 4
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 5
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 5
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 6
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 6
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 7
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 7
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 8
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 8
 			UNION ALL
-			SELECT IFNULL(COUNT(user_id), 0) AS population FROM citizens WHERE user_id = 9; """
+			SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = 9; """
 
 
-d = {}	
+#route for home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	cur = connection.cursor(pymysql.cursors.DictCursor)
@@ -60,29 +52,10 @@ def home():
 	cur = connection.cursor(pymysql.cursors.DictCursor)
 	cur.execute(count1)
 	pop = cur.fetchall()
-	#print(pop)
-	result = zip(res, pop)
-	print(result)#, pop=pop
+	result = zip(res, pop)#zip (join) two query together
 	return render_template('home.html', page_title='Welcome', result = result, pop=pop)
 
-
-@app.route('/about')
-def about():
-	cur = connection.cursor(pymysql.cursors.DictCursor)
-	cur.execute(union_table)
-	res = cur.fetchall()
-	cur.close()
-	
-	cur = connection.cursor(pymysql.cursors.DictCursor)
-	cur.execute(count1)
-	pop = cur.fetchall()
-	#print(pop)
-	result = zip(res, pop)
-	
-	return render_template('about.html', page_title='About', result=result)
-	
-	
-	
+#route for citizenship page for occupy planet	
 @app.route('/citizenship', methods=['GET', 'POST'])
 def citizenship():
 	if request.method == "POST":
@@ -91,19 +64,15 @@ def citizenship():
 		email = request.form.get("email")
 		planet = request.form.get("planet")
 			
-		sql_insert_str = 'INSERT INTO citizens (first_name, last_name, email, user_id) VALUES ("{}", "{}", "{}", "{}");'.format(first_name, last_name, email, planet)
+		sql_insert_str = 'INSERT INTO citizens (first_name, last_name, email, planet_id) VALUES ("{}", "{}", "{}", "{}");'.format(first_name, last_name, email, planet)
 		cur = connection.cursor()
 		cur.execute(sql_insert_str)
 		connection.commit()
-	#	cur.fetchall()
 	else:
 		first_name = ''
 		last_name = ''
 		email = ''
 		planet = '1'
-		print(request.form)
-		print(first_name, last_name, email, planet)
-		
 
 	union_table = """ SELECT * FROM planets JOIN images ON name = picture_name LIMIT 9;"""
 			
@@ -114,30 +83,28 @@ def citizenship():
 	return render_template('citizenship.html', page_title='Citizens_form',result=result
 							,data=(first_name, last_name, email, planet))
 	
-
+#route for project page 
 @app.route('/project', methods=['GET', 'POST'])
 def project():
-	return render_template('project.html', page_title='Project')
-	
-	
-	
-	
-	
-@app.route('/mercury', methods=['GET', 'POST'])
-def mercury():
-	mercury = """ SELECT * FROM planets LEFT JOIN add_info; """
 	cur = connection.cursor(pymysql.cursors.DictCursor)
-	cur.execute(mercury)
-	mer = cur.fetchall()
-	return render_template('mercury.html', page_title='Mercury', mer = mer)
+	cur.execute('SELECT name, dist_from_sun, diameter, moons FROM planets;')
+	beta = cur.fetchall()
+	data = json.dumps(beta)
 
+	return render_template('project.html', page_title='Project', delta=data)
+	
+
+#route for planet details page
 @app.route('/planet-details/<planet_id>', methods=['GET'])
 def planet_details(planet_id):
 	cur = connection.cursor(pymysql.cursors.DictCursor)
-	cur.execute('SELECT * FROM planets INNER JOIN images INNER JOIN add_info ON planets.id = images.picture_id WHERE planets.id = {}'.format(planet_id))
+	cur.execute('SELECT * FROM planets INNER JOIN images JOIN add_info ON planets.id = images.picture_id WHERE  planets.id = {} AND planets.id = add_info.id'.format(planet_id))
 	planet = cur.fetchone()
+	cur.execute('SELECT IFNULL(COUNT(planet_id), 0) AS population FROM citizens WHERE planet_id = {}'.format(planet_id))
+	count = cur.fetchall()
 	
-	return render_template('planet_details.html', planet=planet)
+	return render_template('planet_details.html', page_title='planet details', planet=planet, count=count)
+	
 	
 	
 app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,212 @@
+
+$(".option1").on('click', function(){
+    console.log('change me!');
+});
+//sort bars based on value
+        data = data().sort(function (a, b) {
+            return d3.ascending(a.value, b.value);
+        });
+
+        //set up svg using margin conventions -need plenty of room on the left for labels
+        var margin = {
+            top: 1,
+            right: 25,
+            bottom: 15,
+            left: 100
+        };
+
+        var width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var svg = d3.select("#graphic")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scale.linear()
+            .range([0, width])
+            .domain([0, d3.max(data, function (d) {
+                return (d.dist_from_sun)* 0.9;
+            })]);
+
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
+
+        //make y axis to show bar names
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            //no tick marks
+            .tickSize(0)
+            .orient("left");
+
+        var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g")
+
+        //append rects
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return x(d.dist_from_sun);
+            });
+
+        //add a value label to the right of each bar
+        bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.dist_from_sun) / 2;
+            })
+            .text(function (d) {
+                return d.dist_from_sun;
+            });
+            
+///////////////////////////////////////////
+
+
+        var svg = d3.select("#graphic1")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scale.linear()
+            .range([0, width])
+            .domain([0, d3.max(data, function (d) {
+                return (d.moons);
+            })]);
+
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
+
+        //make y axis to show bar names
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            //no tick marks
+            .tickSize(0)
+            .orient("left");
+
+        var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g")
+
+        //append rects
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return x(d.moons);
+            });
+
+        //add a value label to the right of each bar
+        bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.moons);
+            })
+            .text(function (d) {
+                return d.moons;
+            });
+
+//////////////////////////////////////////
+
+        var svg = d3.select("#graphic2")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scale.linear()
+            .range([0, width])
+            .domain([0, d3.max(data, function (d) {
+                return (d.diameter)* 0.7;
+            })]);
+
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
+
+        //make y axis to show bar names
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            //no tick marks
+            .tickSize(0)
+            .orient("left");
+
+        var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g");
+
+        //append rects
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return x(d.diameter);
+            });
+
+        //add a value label to the right of each bar
+        bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.diameter) / 2;
+            })
+            .text(function (d) {
+                return d.diameter;
+            });   
+
+
