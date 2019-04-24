f369547718b08dc22815ccb247b4841516f64c47
Dimitar Milev <rokambol_88@abv.bg>
2019-04-24 14:26:35 +0000

    add config.py file

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'host': 'cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    'dbname': 'czgiku21kfnsobrv',
+    'user': 'c1pny3zt0ltf9g8t',
+    'password': 'lhfimhgovu3fykt0',
+    'port': 3306
+}
