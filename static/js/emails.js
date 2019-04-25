4944aa8d5fd9ff0e12a1e95aa0f16b25e06c043e
Dimitar Milev <rokambol_88@abv.bg>
2019-04-25 10:30:14 +0000

    update function on js file for redirection on home page

---
+++ static/js/emails.js
@@ -0,0 +1,32 @@
+document.getElementById('insert').addEventListener("click", function(){  
+   var data = {
+    service_id: 'gmail',
+    template_id: 'template_vNFyYhqW',
+    user_id: 'user_InDnzbGp0eCqm9tRk78cx',
+    template_params: {
+        'username': 'James',
+    }
+};
+ 
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+}).done(function() {
+    alert('Your mail is sent!');
+}).fail(function(error) {
+    alert('Oops... ' + JSON.stringify(error));
+});
+  
+}); 
+
+$("document").ready(function(){
+    setTimeout(function(){
+        $("#message").remove();
+    }, 10000 );
+});
+$("document").ready(function(){
+   setTimeout(function(){
+       window.location.replace("https://space-invasion.herokuapp.com/");
+   }, 1000);
+});
